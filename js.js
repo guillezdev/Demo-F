8540e72c7bc9876e13dd4e63bdbf400dcbf80c54
guillezdev <guillezdev@gmail.com>
2023-08-20 10:04:53 -0400

    exemple

--- js.js
+++ js.js
@@ -1,16 +1,15 @@
 const typed = new Typed(".typed", {
   strings: [
-    ".<span class='texts'>Your Apps.</span>",
-    ".<span class='texts'>Your Blog.</span>",
-    ".<span class='texts'>Your Website.</span>",
+    "<span class='texts'>Your Apps.</span>",
+    "<span class='texts'>Your Blog.</span>",
+    "<span class='texts'>Your Website.</span>",
   ],
  loop: "true",
  typeSpeed: 75,
- backSpeed: 200,
- backDelay: 500,
+ backSpeed: 100,
+ backDelay: 100,
  startDelay: 0,
  loop: true,
  loopCount: false,
  smartBackspace:false
-
 });
